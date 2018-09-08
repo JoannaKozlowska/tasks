8c5bac2eae7f5f4056483019b6e0b362db1e61f2
JoannaKozlowska <joanna.kozlowska2016@gmail.com>
2018-09-08 09:48:35 +0200

    Deleting

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,18 @@ public class DbService {
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
+
     public Optional<Task> getTaskById(final Long id) {
         return repository.findById(id);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteById(final Long id) throws TaskNotFoundException {
+        if (repository.findById(id) != null) {
+            repository.delete(id);
+        } else throw new TaskNotFoundException();
     }
 }
